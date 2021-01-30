065fbf6a5e818a67fc66557ae4e8acd68cc09dca
Simon <bardrabbit@gmail.com>
2021-01-30 02:03:00 -0500

    hide search buttons while keep their functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,7 @@ import MicRoundedIcon from '@material-ui/icons/MicRounded';
 import Button from '@material-ui/core/Button';
 import { useHistory } from 'react-router-dom';
 
-function Search() {
+function Search({ hideButtons = false }) {
   const [searchTerm, setSearchTerm] = useState('');
   const history = useHistory();
 
@@ -25,12 +25,29 @@ function Search() {
         />
         <MicRoundedIcon />
       </div>
-      <div className='search__buttons'>
-        <Button onClick={search} variant='outlined' type='submit'>
-          Google Search
-        </Button>
-        <Button variant='outlined'>I am feeling lucky</Button>
-      </div>
+
+      {!hideButtons ? (
+        <div className='search__buttons'>
+          <Button onClick={search} variant='outlined' type='submit'>
+            Google Search
+          </Button>
+          <Button variant='outlined'>I am feeling lucky</Button>
+        </div>
+      ) : (
+        <div className='search__buttons'>
+          <Button
+            className='search__buttonsHidden'
+            onClick={search}
+            variant='outlined'
+            type='submit'
+          >
+            Google Search
+          </Button>
+          <Button className='search__buttonsHidden' variant='outlined'>
+            I am feeling lucky
+          </Button>
+        </div>
+      )}
     </form>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,7 +26,7 @@ function Home() {
           alt='google logo'
         />
         <div className='home__inputContainer'>
-          <Search />
+          <Search hideButtons />
         </div>
       </div>
     </div>
