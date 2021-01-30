3de835ad160ba96f0dc66a0244193fd4570f3db3
Simon <bardrabbit@gmail.com>
2021-01-29 23:54:01 -0500

    create and styling header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
-import AppsIcon from '@material-ui/icons/Apps';
+import AppsOutlinedIcon from '@material-ui/icons/AppsOutlined';
+import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
 
 function Home() {
   return (
     <div className='home'>
       <div className='home__header'>
-        <div className='home__left'>
+        <div className='home__headerLeft'>
           <Link to='/about'>About</Link>
           <Link to='/store'>Store</Link>
         </div>
-        <div className='home__right'>
+        <div className='home__headerRight'>
           <Link to='/gmail'>Gmail</Link>
           <Link to='/images'>Images</Link>
-          <AppsIcon />
+          <AppsOutlinedIcon />
+          <AccountCircleRoundedIcon />
         </div>
       </div>
       <div className='home__body'></div>
