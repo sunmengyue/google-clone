9a15f714cf770dbcc584d779585367d15e5f49fb
Simon <bardrabbit@gmail.com>
2021-01-30 23:21:36 -0500

    add thumbnails

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,7 +26,7 @@ function Home() {
           alt='google logo'
         />
         <div className='home__inputContainer'>
-          <Search hideButtons />
+          <Search />
         </div>
       </div>
     </div>
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -15,8 +15,8 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function SearchPage() {
   const [{ term }, dispach] = useStateValue();
-  //const { data } = useGoogleSearch(term);
-  const data = Response;
+  const { data } = useGoogleSearch(term);
+
   console.log(data);
   return (
     <div className='searchPage'>
@@ -69,7 +69,35 @@ function SearchPage() {
         </div>
       </div>
 
-      <div className='searchPage__results'></div>
+      {term && (
+        <div className='searchPage__results'>
+          <p className='searchPage__resultsCount'>
+            About {data?.searchInformation.totalResults} results{' '}
+            {data?.searchInformation.formattedSearchTime} for {term}
+          </p>
+
+          {data?.items.map((item) => (
+            <div className='searchPage__result'>
+              {item.pagemap?.cse_image?.length > 0 &&
+                item.pagemap?.cse_image[0]?.src && (
+                  <img
+                    className='searchPage__resultImage'
+                    src={
+                      item.pagemap?.cse_image?.length > 0 &&
+                      item.pagemap?.cse_image[0]?.src
+                    }
+                    alt=''
+                  />
+                )}
+              <a href={item.link}>{item.displayLink} ▽</a>
+              <a className='searchPage__resultTitle' href={item.link}>
+                <h2>{item.title}</h2>
+              </a>
+              <p className='searchPage__resultSnippet'>{item.snippet}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
